7b0ad7e159df57aa1e1659a74c24399d73de2b77
ASlicedBread <aslicedbreaaad@gmail.com>
2025-02-06 16:09:05 +0100

    fixed bounce

--- scenes/Ball.cs
+++ scenes/Ball.cs
@@ -15,7 +15,7 @@ public partial class Ball : RigidBody2D
 
     public void resetPosition(bool player1IsWinner){
         Position = new Vector2(bg.Texture.GetWidth()/2, bg.Texture.GetHeight()/2);    
-        velocity = !player1IsWinner ? new Vector2(speed,0) : new Vector2(-speed,0); 
+        velocity = !player1IsWinner ? new Vector2(speed,-speed) : new Vector2(-speed,speed); 
     }
     public override void _Process(double delta) {
         base._Process(delta);
@@ -25,16 +25,22 @@ public partial class Ball : RigidBody2D
         KinematicCollision2D collisionInfo =  MoveAndCollide(new Vector2((float)(velocity.X*delta),(float)(velocity.Y*delta)));
         if(collisionInfo != null)
         {
-            if(collisionInfo.GetCollider() is Player){
-                GD.Print("Base Velocity: "+velocity + " "+ (collisionInfo.GetPosition().Y-GlobalPosition.Y*speed) + " "+ (collisionInfo.GetPosition().Y-GlobalPosition.Y));
-                Vector2 bouncedVel = velocity.Bounce(collisionInfo.GetNormal());
-                velocity.Y =GlobalPosition.Y*1.1f - collisionInfo.GetPosition().Y;
-                velocity.X = bouncedVel.X;
-                GD.Print("Bounced Velocity: "+ velocity);
+            // if(collisionInfo.GetCollider() is Player){
+            //     GD.Print("Base Velocity: "+velocity + " "+ (collisionInfo.GetPosition().Y-GlobalPosition.Y*speed) + " "+ (collisionInfo.GetPosition().Y-GlobalPosition.Y));
+            //     Vector2 bouncedVel = velocity.Bounce(collisionInfo.GetNormal());
+            //     velocity.Y =GlobalPosition.Y*1.1f + bouncedVel.X < 0 ? -collisionInfo.GetPosition().Y : collisionInfo.GetPosition().Y;
+            //     velocity.X = -velocity.X;
+            //     GD.Print("Normalized : " + velocity);
+            //     velocity = new Vector2(velocity.X,(float)
+            //     (velocity.Y/
+            //     (Math.Sqrt(Math.Pow(velocity.X,2)+Math.Pow(velocity.Y,2)))
+            //     *speed));
+            //     GD.Print("Bounced Velocity: "+ velocity);
 
-            }else{
-                velocity = velocity.Bounce(collisionInfo.GetNormal());
-            }
+            // }else{
+            Vector2 bouncedVec = velocity.Bounce(collisionInfo.GetNormal());
+                velocity = new Vector2((float)(bouncedVec.X*1.1),bouncedVec.Y);
+            // }
         }
             
     }
